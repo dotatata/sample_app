09d51757ae3c038ae410b7151695755cecc459ad
dotatata <dotatata@163.com>
2013-08-28 23:31:50 +0800

    update view & css & signup

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
-source 'https://rubygems.org'
+# source 'https://rubygems.org'
+source 'http://ruby.taobao.org/'
 ruby '2.0.0'
 
 gem 'rails', '4.0.0'
+gem 'bootstrap-sass', '2.3.2.0'
 
 group :development, :test do
   gem 'mysql2'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,53 +4,44 @@ describe "StaticPages" do
   # 使用application_helper时 base_title变量在测试用例中无法被正确赋值 只能采用字符串
   # let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
+  subject {page}
+
   describe "HomePage" do
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
-    end
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      # expect(page).to have_title('#{base_title} | Home')
-      expect(page).not_to have_title('| Home')
-    end
+    before {visit root_path}
+    it {should have_content('Sample App')}
+    it {should have_title(full_title(''))}
+    it {should_not have_title(' | Home')}
   end
 
   describe "HelpPage" do
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-    it "should have the right title" do
-      visit '/static_pages/help'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
-    end
+    before {visit help_path}
+    it {should have_content('Help')}
+    it {should have_title(full_title(''))}
   end
 
   describe "AboutPage" do
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
-    it "should have the right title" do
-      visit '/static_pages/about'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
-    end
+    before {visit about_path}
+    it {should have_content('About')}
+    it {should have_title(full_title('About'))}
   end
 
   describe "ContactPage" do
-    it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_content('Contact Us')
-    end
-    it "should have the right title" do
-      visit '/static_pages/contact'
-      # expect(page).to have_title('#{base_title} | Contact')
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Contact')
-    end
+    before {visit contact_path}
+    it {should have_content('Contact')}
+    it {should have_title(full_title('Contact'))}
   end
+
+  # describe "ContactPage" do
+  #   it "should have the content 'Contact'" do
+  #     # visit '/static_pages/contact'
+  #     visit contact_path
+  #     expect(page).to have_content('Contact Us')
+  #   end
+  #   it "should have the right title" do
+  #     # visit '/static_pages/contact'
+  #     visit contact_path
+  #     # expect(page).to have_title('#{base_title} | Contact')
+  #     expect(page).to have_title('Ruby on Rails Tutorial Sample App | Contact')
+  #   end
+  # end
 end
